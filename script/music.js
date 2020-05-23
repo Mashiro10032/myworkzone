100b9440f42b5b83f8e0fd967cb77a37032bbecd
FanJiaWei <965963765@qq.com>
2020-05-24 02:01:45 +0800

    5/24 commit

--- script/music.js
+++ script/music.js
@@ -1,1 +1,20 @@
-var musicName;
+const ap = new APlayer({
+    container: document.getElementById('musicbar'),
+    listFolded:false,
+    listMaxHeight:90,
+    fixed:true,
+    audio: [{
+    name: 'Snow, Snow, Snow',
+    artist: 'Danny Fromajio',
+    url: '../music/Danny Fromajio - Snow, Snow, Snow.mp3',
+    cover: '../images/music/snow.png',
+    theme: '#ebd0c2'
+},{
+    name:'Hotel California',
+    artist:'Eagles',
+    url:'../music/Eagles - Hotel California.mp3',
+    cover:'../images/music/hotel california.png',
+    theme:'#46718b'
+}
+]
+});
--- script/nav.js
+++ script/nav.js
@@ -1,9 +1,11 @@
 //曲线救国,判断当前浏览器是否支持ActiveX控件
 //IE浏览器是不支持的,所以用来判断当前浏览器是否为IE
-$(document).ready(function () {
+
     if (window.ActiveXObject || "ActiveXObject" in window) {
         alert("您当前使用的是IE浏览器,为了能得到更好的浏览体验,请使用Chrome/FireFox或Chrome内核的浏览器浏览")
     };
+
+    // nav阴影
     $("#nav-open").click(function () {
         $("aside").css(
             "box-shadow", "5px 5px 10px 1px #000000"
@@ -15,4 +17,3 @@ $(document).ready(function () {
             "box-shadow", "none"
         );
     });
-})
