84fbe2c35ed641210ce20314ff352940fb75eae4
FanJiaWei <965963765@qq.com>
2020-05-27 23:05:17 +0800

    5-27 23:05 commit

--- src/script/ajax.js
+++ src/script/ajax.js
@@ -15,9 +15,7 @@ function nAjax(){
         success:function(html){
             var result = $(html).find(".content");
             $("section").empty();
-            $("section").html(result);
-            console.log(result);
-            
+            $("section").html(result);   
         }
     });
 };
@@ -34,46 +32,8 @@ function iAjax(){
         },
         success:function(html){
             var result = $(html).find(".content");
+            $("section").empty();
             $("section").html(result);
-            
         }
     })
 }
-//     $.ajax({
-//     async:true,
-//     url:'src/html/nanning.html',
-//     type:'GET',
-//     dataType:'html',
-    
-//     timeout:10000,
-//     error:function(){
-//         alert("页面同步失败,请检查网络");
-//     },
-//     success:function(html){
-//         var result = $(html).find(".content");
-//         $("section").empty();
-//         $("section").html(result);
-//         console.log(result);
-        
-//     }
-// });
-
-// $("li a:eq(0)").click(function(){
-    
-//     $.ajax({
-//     async:true,
-//     url:'index.html',
-//     type:'GET',
-//     dataType:'html',
-    
-//     timeout:10000,
-//     error:function(){
-//         alert("页面同步失败,请检查网络");
-//     },
-//     success:function(html){
-//         var result = $(html).find(".content");
-//         $("section").html(result);
-        
-//     }
-// })
-// });
